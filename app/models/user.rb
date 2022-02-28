6586c8212eb7d1b78529ed7047a12b1afd4df7b0
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-28 19:22:56 +0000

    validations and dot env

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates
+  validates :username, presence: true
   has_many :bookings
   has_many :evaluations
   has_one :service
