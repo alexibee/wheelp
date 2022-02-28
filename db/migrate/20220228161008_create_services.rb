24ef0d892ad03c4eb94111489ceacf6a60b80b39
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-28 16:13:08 +0000

    got rid of the references

--- db/migrate/20220228161008_create_services.rb
+++ db/migrate/20220228161008_create_services.rb
@@ -1,7 +1,6 @@
 class CreateServices < ActiveRecord::Migration[6.1]
   def change
     create_table :services do |t|
-      t.references :user, null: false, foreign_key: true
       t.string :bio
       t.string :address
       t.integer :price
