0d410510ac39783cc9ccddeb74e553c95360ef80
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-03-01 14:25:22 +0000

    validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: (1..5) }
 end
