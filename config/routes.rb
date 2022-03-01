a343ecbe696c15c280798fd46a0d17c9684ecfbc
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-01 15:13:58 +0000

    bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :services, only: %i[index show]
+  resources :services, only: %i[index show] do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[index destroy edit update]
 end
