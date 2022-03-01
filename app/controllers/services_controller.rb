b6ff413238666ee3ccd46205dd7fd91763cf7a0f
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-03-01 14:39:28 +0000

    validations and associations to service model

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,4 +6,27 @@ class ServicesController < ApplicationController
   def show
     @service = Service.find(params[:id])
   end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    # we need `user_id` to associate service with corresponding user?
+    @user = current_user
+    @service.user = @user
+    @service.rating = 1.0
+    if @service.save!
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :price, :description)
+  end
 end
