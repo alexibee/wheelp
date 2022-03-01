cd62c82e2972e495b953c7b6b5889e9adb6ca275
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-01 22:24:45 +0000

    bookings index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services, only: %i[index show new create] do
-    resources :bookings, only: %i[show new create]
+    resources :bookings, only: %i[show new]
   end
-  resources :bookings, only: %i[edit update destroy]
+  resources :bookings, only: %i[index create edit update destroy]
 end
