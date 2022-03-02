e819b1b4ffb69e3baab69496514496eb35bcdb85
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-03-02 12:27:17 +0000

    added availability array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_142055) do
+ActiveRecord::Schema.define(version: 2022_03_02_122511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,11 +85,11 @@ ActiveRecord::Schema.define(version: 2022_03_01_142055) do
     t.string "bio"
     t.string "address"
     t.integer "price"
-    t.date "availability"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "title"
+    t.date "availability", default: [], array: true
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
