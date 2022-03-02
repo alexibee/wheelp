13422e5ee7dca8bd0578c63eadb886fd6f770299
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-02 15:38:42 +0000

    reviews create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,9 +5,9 @@ class ReviewsController < ApplicationController
     @service = @booking.service
     @review = Review.new(review_params)
     @review.user_id = @user.id
-    @review.service_id = @service.id
+    @review.booking_id = @booking.id
     if @review.save
-      redirect_to service_booking_path(@booking)
+      redirect_to service_booking_path(@service, @booking)
     else
       render 'bookings/show'
     end
