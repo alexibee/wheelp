af4b06206aaf7fbcb3d54b99eb43cb58d985e51e
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-03 14:07:17 +0000

    migrations run and added to models

--- app/models/service.rb
+++ app/models/service.rb
@@ -7,6 +7,8 @@ class Service < ApplicationRecord
   validates :title, presence: true
   validates :address, presence: true
   validates :price, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   pg_search_scope :search_by_bio_and_title,
                   against: [:bio, :title], # add :city :category :brands_specialising_on
                   using: {
