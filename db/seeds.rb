92d23fbad99e165d4efa49cca1ac3f2a7f6c3dda
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-03-04 14:13:42 +0000

    seeds phrases

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,18 @@ User.destroy_all
 usernames = ["shaquille_oatmeal", "hanging_with_my_gnomies", "hoosier_daddy", "fast_and_curious", "averagestudent", "BadKarma", "google_was_my_idea", "whats_my_name", "where_am_i", "wheelp4lyf", "mr_lewagon", "ms_lewagon", "806_or_die"]
 names = ["Bobby", "Roy", "Jose", "Alex", "Ismael", "Ibrahim", "Rob", "Dick", "Bill", "Auston", "Alice", "Laura", "Margaret", "Cindy", "Grace"]
 surnames = ["Brown", "Green", "Smith", "Jameson", "Gudbranson", "Velasquez", "Khan", "Goldman", "Lee", "Ozols"]
+experts_photos_male = [
+  "https://c.stocksy.com/a/TW3700/z9/1681841.jpg",
+  "https://www.liveabout.com/thmb/oR_kgnRY47YAD6N5TRlpEYJTCtw=/2064x1161/smart/filters:no_upscale()/GettyImages-522272311-5949bdc15f9b58d58a035319.jpg",
+  "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pmx040118-mechanics002-1521726026.jpg",
+  "https://www.sait.ca/images/Programs%20and%20Courses/Apprenticeships%20and%20Trades/Apprenticeships/motorcycle-mechanic.jpg",
+  "https://i.pinimg.com/564x/b1/db/cc/b1dbccf5b65df457310bbdf8f6ab3fa8.jpg",
+  "https://www.arabnews.pk/sites/default/files/styles/n_670_395/public/2021/07/24/2730671-1458449640.jpg?itok=ntDGNREm",
+  "https://i0.wp.com/ericscarcare.com/wp-content/uploads/car_enthusiast.jpeg?fit=1200%2C900&ssl=1",
+  "https://media.thetab.com/blogs.dir/115/files/2017/05/img-5299.jpg",
+  "https://www.theargus.co.uk/resources/images/13278897.jpg?display=1&htype=0&type=responsive-gallery"
+]
+
 #---------USERS
 customers = []
 experts = []
@@ -35,16 +47,75 @@ usernames.each do |user|
   puts "user #{user} created"
 end
 
+
+#-------- SERVICES
+
 services = []
-adjectives = %w[perfect great amazing awesome exquisite brilliant gorgeous]
-nouns = %w[expert mechanic service]
-address = %w[London Paris Berlin Milan Kairo Greece Latvia Spain Norway]
+
+character_adjectives = %w[helpful wheelpful sincere serious professional truthworthy knowledgeable passionate thoughtful nice]
+vehicles = %w[vehicle car motorbike van four-wheel convertible SUV minivan pickup limousine scooter micro-car familiar]
+vehicle_adjectives = %w[vintage automatic luxury easy-to-drive small low-emission electric]
+good_adjectives = %w[perfect great amazing high-end awesome exquisite brilliant gorgeous helpful sincere]
+categories = %w[expert enthusiast specialist skilled mechanic technician service]
+price_adjectives %w[affordable reasonable inexpensive reduced discounted modest moderate]
+verbs = [
+  "loves",
+  "enjoys",
+  "works with",
+  "owns #{1..4}",
+  "likes"
+]
+timing = [
+  "long time",
+  "amateour",
+  "new",
+  ""
+]
+actions = [
+  "help you find a #{vehicle_adjective} vehicle",
+  "help you find the perfect #{vehicle}",
+  "check the #{vehicle} for you",
+  "verify the condition any #{vehicle} for you",
+  "assist you in your next #{vehicle} purchase",
+  "offering #{price_adjective} priced service",
+  "helps you finding a #{vehicle} at a #{price_adjectives} price"
+]
+wheelp = [
+  "I love offering my service in Wheelp",
+  "I've been a wheelper for a long time now",
+  "I'm new to Wheelp",
+  "I discovered wheelp a couple of months ago",
+  "I used Wheelp a couple of years ago as a customer and a few months ago I decided to become a wheelper",
+  "I joined Wheelp some years ago",
+  "I'm a reputable long time wheelper",
+]
+
+phrase = [
+  "I love helping people in wheelp",
+  "I take this very seriously",
+  "This is a service I actually enjoy doing",
+  "This is the perfect way to get involved with #{vehicles} I like while making some extra cash"
+]
+
+contact_phrase = [
+  "please feel free to get in touch with me",
+  "please don't hesitate to ask me any question",
+  "I would love to check the vehicle for you",
+  "feel free to ask me any question regarding your vehicle or my service",
+  "let me know what you are looking for in your #{vehicle} and book a vehicle viewing with me",
+  "I'm sure I can be super wheelpful to you.",
+  "I'm usually booked but make sure you check my availability to book my service ASAP",
+  "I can only offer my services when I'm not working and unfortunately that is usually not very often",
+  "I am not always available so book a viewing as soon as possible"
+]
+
+addresses = %w[London Dublin Paris Berlin Rome Milan Kairo Latvia Bilbao Sevilla Barcelona Bristol Athens Lion Madrid Norway]
 availability = []
 30.times do |i|
   date = Date.today + i
   availability << date
 end
-(1..10).each do |n|
+(1..20).each do |n|
   unavailability = []
   availability.count.times do |i|
     action = [true, false]
@@ -53,12 +124,21 @@ end
       unavailability << date
     end
   end
+  character = character_adjectives.sample
+  vehicle = vehicles.sample
+  vehicle_adjective = vehicle_adjectives.sample
+  category = categories.sample
+  address = addresses.sample
+  price_adjective = price_adjectives.sample
+  action = actions.sample
+
   new_service = Service.new(
-    bio: "I am a great expert and you will be amazed by how great my services are.",
+    bio: "I am a #{vehicle} #{timing.sample} #{category} who #{verbs.sample} #{vehicle_adjective} vehicles.
+    #{wheelp.sample.capitalize} and I can guarantee that I'm a #{character} #{category}. I love helping people so #{contact_phase}",
     address: address.sample,
     price: rand(45..85),
     availability: availability - unavailability,
-    title: "#{adjectives.sample} #{nouns.sample} to help you find the car of your dreams",
+    title: "#{character} #{vehicle_adjective} #{vehicle} #{category} #{action}",
     user_id: experts.sample
   )
 
