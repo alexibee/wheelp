b862f4cad0c647e9756597c7c77595ce758aa557
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-09 13:00:37 +0000

    evaluation layout done

--- app/javascript/controllers/reveal_controller.js
+++ app/javascript/controllers/reveal_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["reviews", "chat", "link", "rlink", "wrapper"]
+  static targets = ["reviews", "chat", "link", "rlink", "wrapper", "video", "photo", "doc"]
 
   connect() {
 
@@ -33,5 +33,30 @@ export default class extends Controller {
     event.preventDefault()
     this.chatTarget.classList.add('d-none')
     this.linkTarget.classList.remove('d-none')
+  }
+
+  toggleVideo(event) {
+    event.preventDefault()
+    this.videoTarget.classList.toggle('d-none')
+    window.scrollTo({
+      top: this.videoTarget.scrollHeight,
+      behavior: 'smooth'
+    })
+  }
+  togglePhoto(event) {
+    event.preventDefault()
+    this.photoTarget.classList.toggle('d-none')
+    window.scrollTo({
+      top: this.photoTarget.scrollHeight,
+      behavior: 'smooth'
+    })
+  }
+  toggleDoc(event) {
+    event.preventDefault()
+    this.docTarget.classList.toggle('d-none')
+    window.scrollTo({
+      top: this.docTarget.scrollHeight,
+      behavior: 'smooth'
+    })
   }
 }
